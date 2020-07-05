2c0b14b45c6b35d166c04b237c3ad3646a501da9
HerrZwei <2673862757@qq.com>
2020-07-05 12:57:03 +0800

    一些模块的封装

--- src/network/request.js
+++ src/network/request.js
@@ -51,7 +51,7 @@ export function request2(config) {
 
 export function request3(config) {
   const instance = axios.create({
-    baseURL: "http://123.207.32.32:8000",
+    baseURL: "http://152.136.185.210:8000/api/n3",
     timeout: 5000
   })
 
